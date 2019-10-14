be23a0e810d28c1aa7c3d2642799028aee07f9f6
Pierre-Luc Milord <46912935+plmilord@users.noreply.github.com>
2019-10-13 20:22:52 -0400

    Add files via upload

--- custom_components/SpaClient/climate.py
+++ custom_components/SpaClient/climate.py
@@ -5,7 +5,7 @@ from custom_components import SpaClient
 from homeassistant.components.climate import ClimateDevice
 from homeassistant.components.climate.const import (
     SUPPORT_TARGET_TEMPERATURE, HVAC_MODE_OFF)
-from homeassistant.const import ATTR_TEMPERATURE, TEMP_FAHRENHEIT
+from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS, TEMP_FAHRENHEIT
 from homeassistant.util.temperature import convert as convert_temperature
 from datetime import timedelta
 
@@ -38,7 +38,7 @@ class SpaTemp(ClimateDevice):
     @property
     def hvac_modes(self):
         """Return available HVAC modes."""
-        return []
+        return [HVAC_MODE_OFF]
 
     @property
     def supported_features(self):
@@ -58,6 +58,10 @@ class SpaTemp(ClimateDevice):
         _LOGGER.info("Setting Temperature")
         self._spa.send_config_request()
         self._spa.set_temperature(kwargs.get(ATTR_TEMPERATURE))
+
+    def set_hvac_mode(self, hvac_mode):
+        """Set new target hvac mode."""
+        return self._current_operation
 
     @property
     def max_temp(self):
