7d4745394b682958ab8869c0acffabe94af176d3
Pierre-Luc Milord <46912935+plmilord@users.noreply.github.com>
2020-05-09 08:07:03 -0400

    "SpaClient" changed to "spaclient"
    ...to respect usual syntax of component in Home Assistant!

---
+++ custom_components/spaclient/climate.py
@@ -0,0 +1,93 @@
+import logging
+
+# Import the device class from the component that you want to support
+from custom_components import spaclient
+from homeassistant.components.climate import ClimateDevice
+from homeassistant.components.climate.const import (
+    SUPPORT_TARGET_TEMPERATURE, HVAC_MODE_HEAT, HVAC_MODE_OFF)
+from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS, TEMP_FAHRENHEIT
+from homeassistant.util.temperature import convert as convert_temperature
+from datetime import timedelta
+
+_LOGGER = logging.getLogger(__name__)
+SCAN_INTERVAL = spaclient.INTERVAL
+
+SUPPORT_HVAC = [HVAC_MODE_HEAT, HVAC_MODE_OFF]
+SUPPORT_FLAGS = (SUPPORT_TARGET_TEMPERATURE)
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Setup the sensor platform."""
+    spa_data = spaclient.NETWORK
+    async_add_entities([SpaTemp(spa_data)])
+
+class SpaTemp(ClimateDevice):
+    def __init__(self, data):
+        """Initialize the sensor."""
+        self._spa = data.spa
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Spa Temperature'
+
+    @property
+    def hvac_mode(self):
+        """Return current HVAC mode."""
+        if self._spa.get_heating():
+            return HVAC_MODE_HEAT
+        return HVAC_MODE_OFF
+
+    @property
+    def hvac_modes(self):
+        """Return available HVAC modes."""
+        return SUPPORT_HVAC
+
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        return SUPPORT_FLAGS
+
+    @property
+    def current_temperature(self):
+        """Return true if light is on."""
+        return self._spa.get_current_temp()
+
+    @property
+    def target_temperature(self):
+        return self._spa.get_set_temp()
+
+    async def async_set_temperature(self, **kwargs):
+        _LOGGER.info("Setting Temperature")
+        self._spa.send_config_request()
+        self._spa.set_temperature(kwargs.get(ATTR_TEMPERATURE))
+
+    async def async_set_hvac_mode(self, hvac_mode):
+        """Set new target hvac mode."""
+        if self._spa.get_heating():
+            return HVAC_MODE_HEAT
+        return HVAC_MODE_OFF
+
+    @property
+    def max_temp(self):
+        """Return the maximum temperature."""
+        return convert_temperature(104, TEMP_FAHRENHEIT, self.temperature_unit)
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement used by the platform."""
+        return TEMP_FAHRENHEIT
+
+    @property
+    def current_operation(self):
+        """Return current operation ie. heat, cool, idle."""
+        return "Set Temperature"
+
+    async def async_update(self):
+        """Fetch new state data for the sensor."""
+        self._spa.read_all_msg()
+
+    async def async_turn_off(self):
+        pass
+
+    async def async_turn_on(self):
+        pass
---
+++ custom_components/spaclient/switch.py
@@ -0,0 +1,67 @@
+import logging
+
+# Import the device class from the component that you want to support
+from custom_components import spaclient
+from homeassistant.components.switch import SwitchDevice
+from datetime import timedelta
+
+_LOGGER = logging.getLogger(__name__)
+SCAN_INTERVAL = spaclient.INTERVAL
+
+nb_toggle = spaclient.NB_TOGGLE
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Setup the sensor platform."""
+    spa_data = spaclient.NETWORK
+    async_add_entities([SpaPump(1, spa_data), SpaPump(2, spa_data), SpaPump(3, spa_data)])
+
+class SpaPump(SwitchDevice):
+    """Representation of a Sensor."""
+    def __init__(self, pump_num, data):
+        """Initialize the sensor."""
+        self._spa = data.spa
+        self._pump_num = pump_num
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Spa Pump ' + str(self._pump_num)
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes of the device."""
+        attrs = {}
+
+        attrs["Current Temp"] = self._spa.get_current_temp()
+        attrs["Time"] = self._spa.get_current_time()
+        attrs["Set Temp"] = self._spa.get_set_temp()
+        attrs["Pump 1"] = self._spa.get_pump(1)
+        attrs["Pump 2"] = self._spa.get_pump(2)
+        attrs["Pump 3"] = self._spa.get_pump(3)
+        attrs["Temp Range"] = self._spa.get_temp_range()
+
+        return attrs
+
+    @property
+    def is_on(self):
+        """Return true if switch is on."""
+        return self._spa.get_pump(self._pump_num) != "Off"
+
+    async def async_turn_on(self, **kwargs):
+        """Instruct the switch to turn on."""
+        _LOGGER.info("Spa Pump %s status %s", self._pump_num, self._spa.get_pump(self._pump_num))
+        _LOGGER.info("Turning on Spa Pump %s", self._pump_num)
+        self._spa.set_pump(self._pump_num, "High", nb_toggle)
+
+    async def async_turn_off(self, **kwargs):
+        """Instruct the switch to turn off."""
+        _LOGGER.info("Spa Pump %s status %s", self._pump_num, self._spa.get_pump(self._pump_num))
+        if self._spa.get_pump(self._pump_num) == "Low":
+            _LOGGER.info("Turning on Spa Pump %s", self._pump_num)
+        if self._spa.get_pump(self._pump_num) == "High":
+            _LOGGER.info("Turning off Spa Pump %s", self._pump_num)
+        self._spa.set_pump(self._pump_num, "Off", nb_toggle)
+
+    async def async_update(self):
+        """Fetch new state data for the sensor."""
+        self._spa.read_all_msg()
